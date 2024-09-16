90cd8aaedd74aca5d20c76c09ff8b7aa58328b6e
Ville Halonen <71012693+hzville@users.noreply.github.com>
2024-09-16 15:15:24 +0300

    1.05

--- project-todo-app/server.js
+++ project-todo-app/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Express server running')
+  res.send('<h1>Hello from the cluster </h1>')
 })
 
 const PORT = process.env.PORT  || 3001
